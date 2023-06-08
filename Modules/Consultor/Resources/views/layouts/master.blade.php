4fe30cba983eab7b37493af51938c454fc9fd1f6
joseFlorez <gregori.florez@gmail.com>
2023-06-08 12:20:47 -0400

    update

--- Modules/Consultor/Resources/views/layouts/master.blade.php
+++ Modules/Consultor/Resources/views/layouts/master.blade.php
@@ -7,7 +7,7 @@
         <title>Module Consultor</title>
 
        {{-- Laravel Mix - CSS File --}}
-       {{-- <link rel="stylesheet" href="{{ mix('css/consultor.css') }}"> --}}
+       {{-- <link rel="stylesheet" href="{{ asset('css/consultor.css') }}"> --}}
        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
        <link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css" rel="stylesheet">
     </head>
@@ -19,6 +19,6 @@
         </div>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-        <script src="{{ mix('js/app.js') }}"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
